2230f412f5dde067f1909eb768604ab43aac6d8c
unknown <snit.san@mahidol.ac.th>
2022-02-01 12:06:50 +0700

    add TODO 1: to named_route week5

--- labs/5-week/named_route/lib/main.dart
+++ labs/5-week/named_route/lib/main.dart
@@ -11,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Named Route Demo',
+      //TODO 1:  Update Route Table HERE
     );
   }
 }
