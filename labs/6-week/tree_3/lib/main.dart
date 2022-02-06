76de9729517e161d5fba055681296fc9e8d13d03
unknown <snit.san@mahidol.ac.th>
2022-02-06 13:59:40 +0700

    change tree_3 to simple version

--- labs/6-week/tree_3/lib/main.dart
+++ labs/6-week/tree_3/lib/main.dart
@@ -11,54 +11,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: _title,
-      home: MyStatefulWidget(),
+      home: Example(),
     );
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
+class Example extends StatefulWidget {
+  const Example({Key? key}) : super(key: key);
 
   @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+  State<Example> createState() => _ExampleState();
 }
 
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  bool _lightIsOn = false;
+class _ExampleState extends State<Example> {
+  bool change = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: FractionalOffset.center,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(
-                Icons.lightbulb_outline,
-                color: _lightIsOn ? Colors.yellow.shade600 : Colors.black,
-                size: 60,
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  // Toggle light when tapped.
-                  _lightIsOn = !_lightIsOn;
-                });
-              },
-              child: Container(
-                color: Colors.yellow.shade600,
-                padding: const EdgeInsets.all(8),
-                // Change button text when light changes state.
-                child: Text(_lightIsOn ? 'TURN LIGHT OFF' : 'TURN LIGHT ON'),
-              ),
-            ),
-          ],
-        ),
-      ),
+    return Column(
+      children: <Widget>[
+        Expanded(child: change ? _buildWorld() : _buildFlutter())
+      ],
+    );
+  }
+
+  Widget _buildWorld() {
+    return Row(
+      children: <Widget>[Text('Hello World')],
+    );
+  }
+
+  Widget _buildFlutter() {
+    return Row(
+      children: <Widget>[Text('Hello Flutter')],
     );
   }
 }
